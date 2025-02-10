c298edad84244516bd2185ba1f52f4cff81b3644
Prajwal A K <prajwalhassan7@gmail.com>
2025-02-10 20:48:34 +0530

    Added Backend ENV Url

--- config/api.ts
+++ config/api.ts
@@ -1,3 +1,3 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';
-console.log('API_URL:', API_URL);
-console.log('NEXT_PUBLIC_API_URL:', process.env.NEXT_PUBLIC_API_URL);
+// console.log('API_URL:', API_URL);
+// console.log('NEXT_PUBLIC_API_URL:', process.env.NEXT_PUBLIC_API_URL);
